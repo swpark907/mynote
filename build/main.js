2c1d643001a962f51682a1f8425feacad5330c6b
swpark907 <76856321+swpark907@users.noreply.github.com>
2021-11-10 23:42:37 +0900

    make modal errorMsg

--- build/main.js
+++ build/main.js
@@ -1,12 +1,14 @@
 import essayHandler from "./handler.js";
-var selectBox = document.querySelector('#selectBox');
-var openPost = document.querySelector('.open-post');
-var modals = document.querySelector('.modals');
-var modal = document.querySelector('.modal');
-var deleteBtn = document.querySelectorAll('.item-delete');
-var items = document.querySelector('.items');
-var inputTitle = document.querySelector('.input-title');
-var inputDesc = document.querySelector('.input-desc');
+var selectBox = document.querySelector("#selectBox");
+var openPost = document.querySelector(".open-post");
+var modals = document.querySelector(".modals");
+var modal = document.querySelector(".modal");
+var modalTitle = document.querySelector(".modal_title");
+var modalErrorMsg = document.querySelector(".modal_errorMsg");
+var deleteBtn = document.querySelectorAll(".item-delete");
+var items = document.querySelector(".items");
+var inputTitle = document.querySelector(".input-title");
+var inputDesc = document.querySelector(".input-desc");
 var testHandler = new essayHandler();
 var currentType;
 var dateTransform = function (date, symbol) {
@@ -15,43 +17,52 @@ var dateTransform = function (date, symbol) {
     var day = date.getDate();
     return "" + year + symbol + month + symbol + day;
 };
-items === null || items === void 0 ? void 0 : items.addEventListener('click', function (e) {
+items === null || items === void 0 ? void 0 : items.addEventListener("click", function (e) {
     var target = e.target;
-    if (target.className === 'item-delete') {
+    if (target.className === "item-delete") {
         testHandler.deleteItem(target);
     }
 });
-openPost === null || openPost === void 0 ? void 0 : openPost.addEventListener('click', function (e) {
-    console.log(selectBox.selectedOptions[0].label, 'asdf');
+openPost === null || openPost === void 0 ? void 0 : openPost.addEventListener("click", function (e) {
+    console.log(selectBox.selectedOptions[0].label, "asdf");
     var contentType = selectBox.selectedOptions[0].value;
-    if (contentType === 'default') {
+    console.log();
+    if (contentType === "default") {
         return;
     }
-    modal.classList.add('active');
-    modals.style.display = 'block';
+    modalTitle.innerText = selectBox.selectedOptions[0].text;
+    modal.classList.add("active");
+    modals.style.display = "block";
     // 타입별 form 양식이 달라져야함
     currentType = contentType;
 });
-modals.addEventListener('click', function (e) {
+modals.addEventListener("click", function (e) {
     var target = e.target;
-    if (target.className === 'modals' || target.className === 'cancel') {
+    if (target.className === "modals" || target.className === "cancel") {
         e.preventDefault();
-        modals.style.display = 'none';
-        inputTitle.value = '';
-        inputDesc.value = '';
+        modals.style.display = "none";
+        inputTitle.value = "";
+        inputDesc.value = "";
+        modalErrorMsg.style.display = "none";
     }
-    else if (target.className === 'post') {
+    else if (target.className === "post") {
         e.preventDefault();
-        modals.style.display = 'none';
-        testHandler.addItem({
-            type: currentType,
-            id: new Date().getTime(),
-            title: inputTitle.value,
-            content: inputDesc.value,
-            date: dateTransform(new Date(), '-'),
-        });
-        inputTitle.value = '';
-        inputDesc.value = '';
+        if (inputTitle.value == "" || inputDesc.value == "") {
+            modalErrorMsg.style.display = "block";
+        }
+        else {
+            modals.style.display = "none";
+            testHandler.addItem({
+                type: currentType,
+                id: new Date().getTime(),
+                title: inputTitle.value,
+                content: inputDesc.value,
+                date: dateTransform(new Date(), "-"),
+            });
+            modalErrorMsg.style.display = "none";
+        }
+        inputTitle.value = "";
+        inputDesc.value = "";
     }
 });
 // testHandler.addItem({
--- src/main.ts
+++ src/main.ts
@@ -1,75 +1,80 @@
 import essayHandler from "./handler.js";
 
-const selectBox = document.querySelector('#selectBox') as HTMLSelectElement;
-const openPost = document.querySelector('.open-post') as HTMLButtonElement;
-const modals = document.querySelector('.modals') as HTMLElement;
-const modal = document.querySelector('.modal') as HTMLElement;
-const deleteBtn = document.querySelectorAll('.item-delete');
-const items = document.querySelector('.items') as HTMLElement;
-const inputTitle = document.querySelector('.input-title') as HTMLInputElement;
-const inputDesc = document.querySelector('.input-desc') as HTMLInputElement;
-
+const selectBox = document.querySelector("#selectBox") as HTMLSelectElement;
+const openPost = document.querySelector(".open-post") as HTMLButtonElement;
+const modals = document.querySelector(".modals") as HTMLDivElement;
+const modal = document.querySelector(".modal") as HTMLDivElement;
+const modalTitle = document.querySelector(".modal_title") as HTMLElement;
+const modalErrorMsg = document.querySelector(".modal_errorMsg") as HTMLSpanElement;
+const deleteBtn = document.querySelectorAll(".item-delete");
+const items = document.querySelector(".items") as HTMLDivElement;
+const inputTitle = document.querySelector(".input-title") as HTMLInputElement;
+const inputDesc = document.querySelector(".input-desc") as HTMLInputElement;
 
 const testHandler = new essayHandler();
 
-
-type Type = 'essay' | 'youtube' | 'siteLink' | 'music' | 'default';
+type Type = "essay" | "youtube" | "siteLink" | "music" | "default";
 let currentType: Type;
 
 const dateTransform = (date: Date, symbol: string): string => {
   const year = date.getFullYear();
-  const month = date.getMonth() +1 ;
+  const month = date.getMonth() + 1;
   const day = date.getDate();
 
-  return `${year}${symbol}${month}${symbol}${day}`
-}
+  return `${year}${symbol}${month}${symbol}${day}`;
+};
 
-items?.addEventListener('click', (e: Event) => {
+items?.addEventListener("click", (e: Event) => {
   const target = e.target as HTMLDivElement;
-  if(target.className === 'item-delete'){
+  if (target.className === "item-delete") {
     testHandler.deleteItem(target);
-  } 
-})
+  }
+});
 
-openPost?.addEventListener('click', (e) => {  
-  console.log(selectBox.selectedOptions[0].label, 'asdf');
+openPost?.addEventListener("click", (e) => {
+  console.log(selectBox.selectedOptions[0].label, "asdf");
   const contentType: Type = selectBox.selectedOptions[0].value as Type;
+  console.log();
 
-  if(contentType === 'default'){
+  if (contentType === "default") {
     return;
   }
-  modal.classList.add('active');
-  modals.style.display = 'block'
+  modalTitle.innerText = selectBox.selectedOptions[0].text;
+  modal.classList.add("active");
+  modals.style.display = "block";
 
   // 타입별 form 양식이 달라져야함
-  currentType = contentType;  
-})
+  currentType = contentType;
+});
 
-modals.addEventListener('click', (e: Event) => {
+modals.addEventListener("click", (e: Event) => {
   const target = e.target as HTMLDivElement;
-  if(target.className === 'modals' || target.className === 'cancel'){
+  if (target.className === "modals" || target.className === "cancel") {
     e.preventDefault();
-    modals.style.display = 'none';
-    inputTitle.value = '';
-    inputDesc.value = '';
-  } else if(target.className === 'post'){
+    modals.style.display = "none";
+    inputTitle.value = "";
+    inputDesc.value = "";
+    modalErrorMsg.style.display = "none";
+  } else if (target.className === "post") {
     e.preventDefault();
-    modals.style.display = 'none';
+    if (inputTitle.value == "" || inputDesc.value == "") {
+      modalErrorMsg.style.display = "block";
+    } else {      
+      modals.style.display = "none";
+      testHandler.addItem({
+        type: currentType,
+        id: new Date().getTime(),
+        title: inputTitle.value,
+        content: inputDesc.value,
+        date: dateTransform(new Date(), "-"),
+      });
+      modalErrorMsg.style.display = "none";
+    }
     
-    testHandler.addItem({
-      type: currentType,
-      id: new Date().getTime(),
-      title: inputTitle.value,
-      content: inputDesc.value,
-      date: dateTransform(new Date(), '-'),
-    })
-
-    inputTitle.value = '';
-    inputDesc.value = '';
+    inputTitle.value = "";
+    inputDesc.value = "";
   }
-})
-
-
+});
 
 // testHandler.addItem({
 //   type: 'essay',
@@ -78,4 +83,3 @@ modals.addEventListener('click', (e: Event) => {
 //   content: '동적으로 받아온 내용',
 //   date: dateTransform(new Date(), '-')
 // })
-
