cd99a600d823694c2cccae2fa1787733cabd8bff
swpark907 <76856321+swpark907@users.noreply.github.com>
2021-12-14 22:25:56 +0900

    update PageItemComponent in page.ts

--- src/page.ts
+++ src/page.ts
@@ -1,13 +1,34 @@
-export class PageComponent {  
-  private element: HTMLUListElement;
+import { BaseComponent, Component } from './base.js';
+
+export interface Composable {
+  addChild(child: Component): void;
+}
+
+class PageItemComponent extends BaseComponent<HTMLElement> implements Composable{
   constructor(){
-    this.element = document.createElement('ul');
-    this.element.setAttribute('class', 'page');
-    this.element.textContent = 'This is PageComponent'
+    super(`<li class="item" data-type="" id=''>
+            <div class="item-body"></div>
+            <div class='item-bottom'>
+            <button class="item-bottom__delete">삭제</button>
+            <button class="item-bottom__update">수정</button>
+            </div>
+          </li>`)
+  }
+  addChild(child: Component){
+    const container = this.element.querySelector('.item-body')! as HTMLElement;
+    child.attachTo(container);
+  }
+}
+
+export class PageComponent extends BaseComponent<HTMLUListElement> implements Composable{  
+  constructor(){
+    super('<ul></ul>');
   }
 
-  attachTo(parent: HTMLElement, position: InsertPosition = 'afterbegin'){
-    parent.insertAdjacentElement(position, this.element);
+  addChild(section: Component){
+    const item = new PageItemComponent();
+    item.addChild(section);
+    item.attachTo(this.element, 'beforeend');
   }
   
 }
